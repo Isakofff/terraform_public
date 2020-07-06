f538b71b853ec59c6bbe62c85554eabf8960ca44
Anton Isakov <anton.i@scopicsoftware.com>
2020-07-06 10:31:50 +0700

    refactoring

--- terraform_template/connections.tf
+++ terraform_template/connections.tf
@@ -1,6 +1,5 @@
 provider "aws" {
   region = var.main-region
-  # you can add keys here
-  #access_key = var.AK
-  #secret_key = var.SK
+  access_key = var.AK
+  secret_key = var.SK
 }
---
+++ terraform_template/storage.tf
@@ -0,0 +1,4 @@
+resource "aws" "bucket1" {
+  bucket = "project-name-2020"
+  acl    = "private"
+}
