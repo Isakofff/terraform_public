fd674f6ee054b4ce050977a9211d9afa6ec9aaa7
Anton Isakov <ping115115@gmail.com>
2020-08-05 20:11:10 +0700

    refactoring

--- terraform_template/main.tf
+++ terraform_template/main.tf
@@ -9,20 +9,20 @@ module "aws-s3" {
 
   bucket_name = var.bucket_name
 }
-//
-//module "aws-vpc" {
-//  source = "./modules/network"
-//
-//  AZs         = var.AZs
-//}
-//
-//module "aws-instances" {
-//  source = "./modules/instances"
-//
-//  golden_ami          = var.golden_ami
-//  key_name            = var.key_name
-//  AZs                 = var.AZs
-//  dev-list-of-target  = var.dev-list-of-target
-//  subnet1             = module.aws-vpc.subnet1
-//  security_group      = module.aws-vpc.security_group
-//}
+
+module "aws-vpc" {
+  source = "./modules/network"
+
+  AZs         = var.AZs
+}
+
+module "aws-instances" {
+  source = "./modules/instances"
+
+  golden_ami          = var.golden_ami
+  key_name            = var.key_name
+  AZs                 = var.AZs
+  dev-list-of-target  = var.dev-list-of-target
+  subnet1             = module.aws-vpc.subnet1
+  security_group      = module.aws-vpc.security_group
+}
