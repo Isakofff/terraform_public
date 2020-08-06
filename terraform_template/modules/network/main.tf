fb7214c1ac5bd19d5945e93646ba4ae21d80b545
Anton Isakov <ping115115@gmail.com>
2020-08-06 20:47:01 +0700

    configured network module

--- terraform_template/modules/network/main.tf
+++ terraform_template/modules/network/main.tf
@@ -7,12 +7,20 @@ resource "aws_vpc" "environment1" {
   }
 }
 
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.environment1.id
+  tags = {
+    Name = "terraform-gw"
+  }
+}
+
 resource "aws_subnet" "subnet1" {
   # https://www.terraform.io/docs/configuration/functions/cidrsubnet.html
   cidr_block = cidrsubnet(aws_vpc.environment1.cidr_block, 3, 1)
   # call our VPC by ID
   vpc_id            = aws_vpc.environment1.id
   availability_zone = var.AZs["az1"]
+  map_public_ip_on_launch = true
   tags = {
     Name = "terraform-aws-subnet1"
   }
@@ -25,6 +33,24 @@ resource "aws_subnet" "subnet2" {
   tags = {
     Name = "terraform-aws-subnet2"
   }
+}
+
+resource "aws_route_table" "rt-public" {
+  vpc_id = aws_vpc.environment1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "Public Subnet"
+  }
+}
+
+resource "aws_route_table_association" "rta-public" {
+  subnet_id = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.rt-public.id
 }
 
 resource "aws_security_group" "subnet-security" {
@@ -40,6 +66,12 @@ resource "aws_security_group" "subnet-security" {
     # 0.0.0.0/0 - worldwide
     # ["ip/32", "ip/32"]
     cidr_blocks = [aws_vpc.environment1.cidr_block]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port   = 80
