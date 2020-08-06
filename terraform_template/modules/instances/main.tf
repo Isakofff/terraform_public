e8cc0fd7d4a96daffb0cbd1fd457441ceac8afbc
Anton Isakov <ping115115@gmail.com>
2020-08-06 20:47:45 +0700

    added instance

--- terraform_template/modules/instances/main.tf
+++ terraform_template/modules/instances/main.tf
@@ -1,10 +1,11 @@
 # EC2
 resource "aws_instance" "main-server" {
   ami = var.golden_ami
-  instance_type = "t3a.medium"
+  instance_type = "t2.micro"
   key_name = var.key_name
   vpc_security_group_ids = [var.security_group]
   subnet_id = var.subnet1
+  count = 2
 
   root_block_device {
     volume_type = "standard"
@@ -17,10 +18,15 @@ resource "aws_instance" "main-server" {
   }
 }
 
+# decoupling the creation of IP and its assignment
+resource "aws_eip_association" "eip_association" {
+  instance_id = aws_instance.main-server[0].id
+  allocation_id = aws_eip.main-server-ip.id
+}
 
 # static IP
 resource "aws_eip" "main-server-ip" {
-  instance = aws_instance.main-server.id
+  instance = aws_instance.main-server[0].id
 
   tags = {
     Name = "terraform-aws-main-server-ip"
