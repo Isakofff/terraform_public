1ae2789fc5b54e1302d0aa128b6983d81418ad57
Anton Isakov <ping115115@gmail.com>
2020-08-06 20:48:00 +0700

    refactoring

--- terraform_template/run.tf
+++ terraform_template/run.tf
@@ -6,19 +6,16 @@ provider "aws" {
 
 module "aws-s3" {
   source = "./modules/storage"
-
   bucket_name = var.bucket_name
 }
 
 module "aws-vpc" {
   source = "./modules/network"
-
   AZs         = var.AZs
 }
 
 module "aws-instances" {
   source = "./modules/instances"
-
   golden_ami          = var.golden_ami
   key_name            = var.key_name
   AZs                 = var.AZs
